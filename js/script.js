ec792c68fe91c557970dd144660017990b071795
valeriofilareti <valerio.filareti@gmail.com>
2023-03-23 14:27:30 +0100

    [add] random number

--- js/script.js
+++ js/script.js
@@ -1,24 +1,30 @@
-const mailAutorizzate = [
-  'ad@gm.com',
-  'il@gm.com',
-  'sy@gm.com',
-  'fi@gm.com',
-  'ed@gm.com'
-]
+// const mailAutorizzate = [
+//   'ad@gm.com',
+//   'il@gm.com',
+//   'sy@gm.com',
+//   'fi@gm.com',
+//   'ed@gm.com'
+// ]
 
 //const userMail = prompt('inserisci la tua mail')
 
-console.log(userMail);
+// console.log(userMail);
 
-console.log(mailAutorizzate);
+// console.log(mailAutorizzate);
 
-for(i = 0; i<mailAutorizzate.length; i++) {
-  if(userMail === mailAutorizzate[i]){
-    console.log('yes')
-  }
-}
+// for(i = 0; i<mailAutorizzate.length; i++) {
+//   if(userMail === mailAutorizzate[i]){
+//     console.log('yes')
+//   }
+// }
+
+
+
 
 // dadi
 
 const dadoFacce = [1, 2, 3, 4, 5, 6];
 
+const num = Math.floor(Math.random() * dadoFacce.length);
+
+console.log(num);
